43ea36d50052c396df383d57c9c385e30c7ac6ef
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2023-02-07 08:39:29 +0100

    router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Home from './views/Home'
+import Cart from './views/Cart'
+
 export default function App (): JSX.Element {
   return (
-    <h1 className='text-3xl font-bold underline'>Hello world</h1>
+    <BrowserRouter>
+      <Navbar />
+
+      <Switch>
+        <Route path='/cart' component={Cart} />
+        <Route path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
   )
 }
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar (): JSX.Element {
+  return (
+    <header>
+      <h1>Tienda online</h1>
+    </header>
+  )
+}
