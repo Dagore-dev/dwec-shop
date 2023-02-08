f925f189b8c5739b7c57d82d7bc249d11319c57d
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2023-02-08 10:05:35 +0100

    fake products hardcode

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -5,7 +5,144 @@ export default function Home (): JSX.Element {
     <>
       <h2 className='text-4xl'>Novedades</h2>
 
-      <ProductsGrid listOfProducts={[]} />
+      <ProductsGrid listOfProducts={[
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        },
+        {
+          name: 'Producto 1',
+          description: 'Este es el producto 1',
+          imgAlt: 'Imagen no disponible',
+          imgSrc: '',
+          uploadDate: 'Hace 4 días',
+          comments: 'Última versión',
+          warranty: '2 años'
+        }
+      ]}
+      />
     </>
   )
 }
